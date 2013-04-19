2fedce7ecf5cec97891fd89f0b3467a7fcd6401e
leizhang <leizhang@thoughtworks.com>
2013-04-19 15:49:37 +0800

    add servlet test

---
+++ src/test/java/com/thoughtworks/maomao/web/DispatchServlet.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.maomao.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+public class DispatchServlet extends HttpServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+    }
+}
